c5e44ca29ff2bed99016649eac78dfe5cd1da737
chinhtrieudong <lebachinh2002@gmail.com>
2024-09-24 10:37:33 +0700

    Code delete Lesson UI

--- src/views/lesson/LessonsPage.vue
+++ src/views/lesson/LessonsPage.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="d-flex mt-3 justify-content-between align-items-center">
-    <router-link
-      :to="`/chapters?idCourse=${idCourse}`"
-      class="text-decoration-none"
-    >
+    <router-link :to="`/chapters?idCourse=${idCourse}`" class="text-decoration-none">
       <div class="d-flex align-items-center gap-2">
         <i class="fa-solid fa-arrow-left text-dark"></i>
         <p class="mb-0 text-dark">Danh sách các chương</p>
@@ -14,20 +11,20 @@
       <button type="button" class="btn btn-primary">Sắp xếp bài tập</button>
     </div>
   </div>
-  <Table
-    :header="header"
-    :data="data.assignments"
-    :keys="keys"
-    :actions="actions"
-  ></Table>
+  <Table :header="header" :data="data.assignments" :keys="keys" :actions="actions" @delete-item="deleteLesson"></Table>
+  <b-modal v-model="isModalVisible" title="Xác nhận xóa" ok-title="Xóa" cancel-title="Đóng" ok-variant="danger"
+    @ok="handleDelete">
+    <p>Bạn có chắc chắn muốn xóa bài tập không?</p>
+  </b-modal>
 </template>
 
 <script setup>
 import axios from "axios";
 import { reactive } from "vue";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import Table from "@/components/Tables/Table.vue";
+import { toast } from "vue3-toastify";
 
 const rootAPI = process.env.VUE_APP_ROOT_API;
 
@@ -39,6 +36,9 @@ const idCourse = route.query.idCourse;
 const data = reactive({
   assignments: [],
 });
+
+const isModalVisible = ref(false);
+const itemToDelete = ref();
 
 const header = ["STT", "Tên Bài tập", "Hành động"];
 const keys = ["title"];
@@ -78,6 +78,26 @@ const fetchAssignments = async () => {
   } catch (error) {
     console.log(error);
   }
+};
+
+// xử lý xóa bài tập
+const handleDelete = async () => {
+  try {
+    await axios.delete(`${rootAPI}/lessons/${itemToDelete.value.id}`);
+    data.assignments = data.assignments.filter(
+      (item) => item.id !== itemToDelete.value.id
+    );
+    isModalVisible.value = false;
+    toast.success("Xóa chương thành công");
+  } catch (error) {
+    console.log(error);
+    toast.error("Có lỗi xảy ra");
+  }
+};
+
+const deleteLesson = (chapter) => {
+  isModalVisible.value = true;
+  itemToDelete.value = chapter;
 };
 
 onMounted(async () => {
