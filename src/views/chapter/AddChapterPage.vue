b70518f28743fa00d0234da5cfdcc58efa573e72
Pham Thanh Truc <kientruc582@gmail.com>
2024-09-27 17:40:38 +0700

    fix conflict

--- src/views/chapter/AddChapterPage.vue
+++ src/views/chapter/AddChapterPage.vue
@@ -1,11 +1,6 @@
 <template>
-  <div
-    class="d-flex mt-3 justify-content-between align-items-center chapter-header"
-  >
-    <router-link
-      :to="{ path: '/chapters', query: { idCourse: idCourse } }"
-      class="text-decoration-none"
-    >
+  <div class="d-flex mt-3 justify-content-between align-items-center chapter-header">
+    <router-link :to="{ path: '/chapters', query: { idCourse: idCourse } }" class="text-decoration-none">
       <div class="d-flex align-items-center gap-2">
         <i class="fa-solid fa-arrow-left text-dark"></i>
         <p class="mb-0 text-dark">Danh sách chương</p>
@@ -46,37 +41,27 @@
       </div>
     </div>
 
+    <div class="col-7">
+      <h2>Thêm Chương Mới</h2>
+      <form @submit.prevent="addChapter">
+        <div class="mb-3">
+          <label for="chapterName" class="form-label">Tên chương</label>
+          <input type="text" class="form-control" id="chapterName" v-model="chapterName" required />
+        </div>
 
-        <div class="col-7">
-            <h2>Thêm Chương Mới</h2>
-            <form @submit.prevent="addChapter">
-                <div class="mb-3">
-                    <label for="chapterName" class="form-label">Tên chương</label>
-                    <input type="text" class="form-control" id="chapterName" v-model="chapterName" required />
-                </div>
-
-                <div class="mb-3">
-                    <label for="isPublic" class="form-label">Chế độ</label>
-                    <select v-model="isPublic" class="form-select" aria-label="Công khai">
-                        <option v-for="option in publicOptions" :key="option.value" :value="option.value">
-                            {{ option.text }}
-                        </option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="supporter" class="form-label">Người hổ trợ</label>
-                    <Multiselect
-                    v-model="mentors.data"
-                    :options="options.data"
-                    label="name"
-                    :multiple="true"
-                    :taggable="true"
-                    @tag="addTeacher"
-                    @remove="removeTeacher"
-                    :close-on-select="false"
-                    placeholder="Chọn người hổ trợ"
-                    />
-                </div>
+        <div class="mb-3">
+          <label for="isPublic" class="form-label">Chế độ</label>
+          <select v-model="isPublic" class="form-select" aria-label="Công khai">
+            <option v-for="option in publicOptions" :key="option.value" :value="option.value">
+              {{ option.text }}
+            </option>
+          </select>
+        </div>
+        <div class="mb-3">
+          <label for="supporter" class="form-label">Người hổ trợ</label>
+          <Multiselect v-model="mentors.data" :options="options.data" label="name" :multiple="true" :taggable="true"
+            @tag="addTeacher" @remove="removeTeacher" :close-on-select="false" placeholder="Chọn người hổ trợ" />
+        </div>
         <button type="submit" class="btn btn-primary">Thêm Chương</button>
       </form>
     </div>
@@ -96,11 +81,11 @@ const chapterName = ref("");
 const isPublic = ref(true);
 
 const mentors = reactive({
-    data:[]
-})
+  data: [],
+});
 const options = reactive({
-    data:[]
-})
+  data: [],
+});
 
 const publicOptions = [
   { value: true, text: "Công khai" },
@@ -112,28 +97,29 @@ const idCourse = route.query.idCourse;
 const existingChapters = ref([]);
 
 const addChapter = async () => {
-    try {
-        const newChapter = {
-            name: chapterName.value,
-            isPublic: isPublic.value,
-            courseId: idCourse,
-            mentor: mentors.data
-        };
-        const response = await axios.post(`${process.env.VUE_APP_ROOT_API}/chapters`, newChapter);
+  try {
+    const newChapter = {
+      name: chapterName.value,
+      isPublic: isPublic.value,
+      courseId: idCourse,
+      mentor: mentors.data,
+    };
+    const response = await axios.post(
+      `${process.env.VUE_APP_ROOT_API}/chapters`,
+      newChapter
+    );
 
-        toast.success("Thêm chương thành công", {
-            position: "top-right",
-            autoClose: 3000,
-        });
-
-    } catch (error) {
-        toast.error("Thêm chương thất bại", {
-            position: "top-right",
-            autoClose: 3000,
-        });
-        console.error('Thêm chương thất bại:', error);
-    }
-
+    toast.success("Thêm chương thành công", {
+      position: "top-right",
+      autoClose: 3000,
+    });
+  } catch (error) {
+    toast.error("Thêm chương thất bại", {
+      position: "top-right",
+      autoClose: 3000,
+    });
+    console.error("Thêm chương thất bại:", error);
+  }
 };
 
 const dataCourse = reactive({
@@ -152,41 +138,35 @@ const fetchChapters = async () => {
 };
 
 const fetchCourse = async () => {
-
-    try {
-        const response = await axios.get(`${rootAPI}/courses/${idCourse}`);
-        dataCourse.course = response.data.data;
-        // dataCourse.course = {...dataCourse.course, mentor:[]}
-        console.log("dataCourse", dataCourse.course);
-
-    } catch (error) {
-        console.error(error);
-    }
-}
-
+  try {
+    const response = await axios.get(`${rootAPI}/courses/${idCourse}`);
+    dataCourse.course = response.data.data;
+    // dataCourse.course = {...dataCourse.course, mentor:[]}
+    console.log("dataCourse", dataCourse.course);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const fetchMentors = async () => {
-    try {
-        const response = await axios.get(`${rootAPI}/mentors`);
-        options.data = response.data.data.items;
-    }catch(err){
-        console.log(err);
-    }
-}
+  try {
+    const response = await axios.get(`${rootAPI}/mentors`);
+    options.data = response.data.data.items;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const removeTeacher = (tagToRemove) => {
-    const teacherSet = new Set(mentors.data);
-    teacherSet.delete(tagToRemove);
-    mentors.data = Array.from(teacherSet);
-
+  const teacherSet = new Set(mentors.data);
+  teacherSet.delete(tagToRemove);
+  mentors.data = Array.from(teacherSet);
 };
 
 onMounted(async () => {
-
-    await fetchMentors();
-    await fetchChapters();
-    await fetchCourse();
-
+  await fetchMentors();
+  await fetchChapters();
+  await fetchCourse();
 });
 </script>
 
