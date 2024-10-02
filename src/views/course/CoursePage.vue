c36574d73fc5fe97a04160efe8d77d3519a7b7a5
Pham Thanh Truc <kientruc582@gmail.com>
2024-10-02 15:02:28 +0700

    fix: conflict when merge feature/588

--- src/views/course/CoursePage.vue
+++ src/views/course/CoursePage.vue
@@ -2,7 +2,6 @@
   <div class="d-flex justify-content-between align-items-center my-4 container">
     <p class="course-list-title">Danh sách khóa học</p>
     <button class="btn btn-primary create-course-btn align-items-center" @click="createCourse">
-    <button class="btn btn-primary create-course-btn align-items-center" @click="createCourse">
       Thêm mới
     </button>
   </div>
